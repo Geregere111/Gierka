0f19aeb577f96ddde0077d70d85aecc76ee45f02
DavidSolz <308148@office.umcs.pl>
2024-08-19 23:06:32 +0200

    more robust approach and logging to console

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
 
-    public void StartGame(){
-        Debug.Log("Loading next scene...");
+    [SerializeField]
+    private int settingsSceneIndex = 1;
+
+    public void StartGame()
+    {
+        Debug.Log("Loading game scene ...");
     }
 
-    public void OpenSettings(){
-        SceneManager.LoadScene(1);
+    public void OpenSettings()
+    {
+
+        if (settingsSceneIndex >= 0 && settingsSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("Loading settings scene with index: " + settingsSceneIndex);
+            SceneManager.LoadScene(settingsSceneIndex);
+        }
+        else
+        {
+            Debug.LogError("Settings scene index is out of range or invalid.");
+        }
     }
 
-    public void CloseWindow(){
+    public void CloseWindow()
+    {
+
+        Debug.Log("Quitting application...");
         Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 
 }
--- Assets/SettingsMenuController.cs
+++ Assets/SettingsMenuController.cs
@@ -2,46 +2,85 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SettingsControllerScript : MonoBehaviour
+public class SettingsMenuController : MonoBehaviour
 {
+    private Resolution[] resolutions;
 
-    Resolution[] resolutions;
+    [SerializeField]
+    private Dropdown resolutionDropdown;
 
-    public Dropdown resolutionDropdown;
+    [SerializeField]
+    private int mainMenuSceneIndex = 0;
 
     void Start()
     {
+        if (resolutionDropdown == null)
+        {
+            Debug.LogError("Resolution dropdown is not assigned in the Inspector.");
+            return;
+        }
 
         resolutions = Screen.resolutions;
 
-        resolutionDropdown.ClearOptions();
-        resolutionDropdown.options = resolutions.Select(x => new Dropdown.OptionData($"{x.width} x {x.height}")).ToList();
-        resolutionDropdown.RefreshShownValue();
+        if (resolutions == null || resolutions.Length == 0)
+        {
+            Debug.LogError("No screen resolutions found.");
+            return;
+        }
 
+        resolutionDropdown.ClearOptions();
+
+        var options = resolutions.Select(x => new Dropdown.OptionData($"{x.width} x {x.height}")).ToList();
+        resolutionDropdown.AddOptions(options);
+
+        var currentResolutionIndex = Array.FindIndex(resolutions, r => r.width == Screen.currentResolution.width && r.height == Screen.currentResolution.height);
+        if (currentResolutionIndex != -1)
+        {
+            resolutionDropdown.value = currentResolutionIndex;
+        }
+        else
+        {
+            Debug.LogWarning("Current screen resolution not found in available resolutions list.");
+        }
+
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void SetResolution(int resolutionIndex)
     {
-        Debug.Assert(resolutionIndex <= resolutions.Length, "Resolution out of bound");
-
-        var resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        if (resolutionIndex >= 0 && resolutionIndex < resolutions.Length)
+        {
+            var resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+            Debug.Log($"Resolution set to {resolution.width} x {resolution.height}");
+        }
+        else
+        {
+            Debug.LogError("Selected resolution index is out of bounds.");
+        }
     }
 
     public void ToggleFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        Debug.Log($"Fullscreen mode set to {isFullscreen}");
     }
 
     public void ReturnToMainMenu()
     {
-        SceneManager.LoadScene(0);
-    }
 
+        if (mainMenuSceneIndex >= 0 && mainMenuSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(mainMenuSceneIndex);
+        }
+        else
+        {
+            Debug.LogError("Main menu scene index is out of range or invalid.");
+        }
+    }
 }
